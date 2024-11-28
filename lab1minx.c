a8573222ee43bb93e6e3c0be53634209f687830a
6704062617067 <s6704062617067@email.kmutnb.ac.th>
2024-11-28 18:10:23 +0700

    scanF Finish

--- lab1minx.c
+++ lab1minx.c
@@ -1,11 +1,22 @@
 #include<stdio.h>
 int main() {
-  	int num1=100;
-  	int num2=50;
-  	if (num1>num2) {
-  		printf("num1 is greater than num2");
-  	} else {
-	  
-  		printf("num2 is greater than num1");
+  	int num1;
+	int num2;
+	
+	printf("n1 :");	
+	scanf("%d", &num1);	
+	printf("n2 :");	
+	scanf("%d", &num2);
+	
+	if(num1>num2){
+		printf("num1 is greater than num2");
+		//diff
+		printf("\n%d",(num1-num2));
+	} else {
+		printf("num2 is greater than num1");
+		//diff
+		printf("\n%d",(num2-num1));
 	}
+	
+
 }

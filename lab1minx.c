14b508b58b3673dbde151dd8b5760209a3a5dbdd
6704062617067 <s6704062617067@email.kmutnb.ac.th>
2024-11-28 18:28:03 +0700

    n1 n2 สองบรรทัด

--- lab1minx.c
+++ lab1minx.c
@@ -3,8 +3,9 @@ int main() {
   	int num1; 
 	int num2;
 	
-	printf("n1 and n2:");	
-	scanf("%d %d", &num1 , &num2);	
+	printf("n1 enter n2:");	
+	scanf("%d", &num1);	
+	scanf("%d", &num2);
 	
 	if(num1>num2){
 		printf("num1 is greater than num2");
